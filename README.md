6c5487686baca75f76aca3b4c4170ab4d13e33dd
Zeerek Ahmad <zeerek@polymathrobotics.com>
2022-12-16 20:09:33 +0000

    Formatting upgrades and adding some readme

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # ros2_behavior_tree_example
 Toy example for running a simple behavior tree with ROS2
+
+# Launch Arguments to Launch the Test Nodes
+```bash
+> ros2 launch ros2_behavior_tree_example ping_pong_launch.py -s
+
+Arguments (pass arguments as '<name>:=<value>'):
+
+    'node1_enable':
+        Enable Secondary Node in case you want to launch separately
+        (default: 'True')
+
+    'node2_enable':
+        Enable Secondary Node in case you want to launch separately
+        (default: 'True')
+
+    'node1_mode':
+        Set trees to reactive sequence. Valid choices are: ['sequence', 'reactive_sequence']
+        (default: 'reactive_sequence')
+
+    'node2_mode':
+        Set trees to standard sequence. Valid choices are: ['sequence', 'reactive_sequence']
+        (default: 'sequence')
+
+    'node1_behaviortree':
+        Set behevior tree file to use desired nodes. Valid choices are: ['ping_pong.xml', 'ping_pong_no_decorator.xml', 'ping_pong_executor.xml']
+        (default: 'ping_pong_executor.xml')
+
+    'node2_behaviortree':
+        Set behavior tree file to use desired nodes. Valid choices are: ['ping_pong.xml', 'ping_pong_no_decorator.xml', 'ping_pong_executor.xml']
+        (default: 'ping_pong_no_decorator.xml')
+```
--- launch/ping_pong_launch.py
+++ launch/ping_pong_launch.py
@@ -1,17 +1,48 @@
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
-from launch.conditions import LaunchConfigurationNotEquals
+from launch.actions import DeclareLaunchArgument
+from launch.conditions import IfCondition
 from launch_ros.actions import Node
 
 from ament_index_python.packages import get_package_share_directory
 
-from launch.substitutions import LaunchConfiguration, PythonExpression, PathJoinSubstitution
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from pathlib import Path
+
+import pdb
 
 def generate_launch_description():
     package_dir = Path(get_package_share_directory('ros2_behavior_tree_example'))
 
+    mode_choices = ["sequence", "reactive_sequence"]
+    tree_choices = ["ping_pong.xml", "ping_pong_no_decorator.xml", "ping_pong_executor.xml"]
+
+    node1_enable_arg = DeclareLaunchArgument("node1_enable", 
+                                            default_value="True", 
+                                            description="Enable Secondary Node in case you want to launch separately")
+    node2_enable_arg = DeclareLaunchArgument("node2_enable", 
+                                            default_value="True", 
+                                            description="Enable Secondary Node in case you want to launch separately")
+
+    node1_mode_arg = DeclareLaunchArgument("node1_mode", 
+                                            default_value="reactive_sequence", 
+                                            choices=mode_choices, 
+                                            description="Set trees to reactive sequence")
+    node2_mode_arg = DeclareLaunchArgument("node2_mode", 
+                                            default_value="sequence", 
+                                            choices=mode_choices, 
+                                            description="Set trees to standard sequence")
+
+    node1_behaviortree_arg = DeclareLaunchArgument("node1_behaviortree", 
+                                                    default_value="ping_pong_executor.xml", 
+                                                    choices=tree_choices, 
+                                                    description="Set behevior tree file to use desired nodes")
+    node2_behaviortree_arg = DeclareLaunchArgument("node2_behaviortree", 
+                                                    default_value="ping_pong_no_decorator.xml", 
+                                                    choices=tree_choices, 
+                                                    description="Set behavior tree file to use desired nodes")
+
     primary_ping_pong_node = Node(
+        condition=IfCondition(LaunchConfiguration("node1_enable")),
         package='ros2_behavior_tree_example',
         executable='behavior_tree_example',
         name='primary_ping_pong',
@@ -21,11 +52,14 @@ def generate_launch_description():
             "rate_hz" : 1.0,
             "num_republish": 5,
             "ping_starter" : True,
-            "behaviortree_file" : str(package_dir / "behavior_trees"/'reactive_sequence'/'ping_pong_no_decorator.xml')
+            "behaviortree_file" : PathJoinSubstitution([str(package_dir / "behavior_trees"),
+                                                        LaunchConfiguration("node1_mode"),
+                                                        LaunchConfiguration("node1_behaviortree")])
         }]
     )
 
     secondary_ping_pong_node = Node(
+        condition=IfCondition(LaunchConfiguration("node2_enable")),
         package='ros2_behavior_tree_example',
         executable='behavior_tree_example',
         name='secondary_ping_pong',
@@ -35,11 +69,19 @@ def generate_launch_description():
             "rate_hz" : 0.75,
             "num_republish": 4,
             "ping_starter" : False,
-            "behaviortree_file" : str(package_dir / "behavior_trees"/'reactive_sequence'/'ping_pong.xml')
+            "behaviortree_file" : PathJoinSubstitution([str(package_dir / "behavior_trees"),
+                                                        LaunchConfiguration("node2_mode"),
+                                                        LaunchConfiguration("node2_behaviortree")])
             }]
     )
 
     return LaunchDescription([
+        node1_enable_arg,
+        node2_enable_arg,
+        node1_mode_arg,
+        node2_mode_arg,
+        node1_behaviortree_arg,
+        node2_behaviortree_arg,
         primary_ping_pong_node,
         secondary_ping_pong_node
     ])
