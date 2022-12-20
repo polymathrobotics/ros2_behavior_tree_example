4e04ecdd88fdfaa871dcfe8d401a6d101a9d808c
Zeerek <zeerek@polymathrobotics.com>
2022-12-20 23:47:55 +0000

    Adding bt ros node as a component and groot compatible xml

--- src/bt_ros_node.cpp
+++ src/bt_ros_node.cpp
@@ -123,3 +123,10 @@ namespace bt_ros_example
     }
 
 }
+
+#include "rclcpp_components/register_node_macro.hpp"
+
+// Register the component with class_loader.
+// This acts as a sort of entry point, allowing the component to be discoverable when its library
+// is being loaded into a running process.
+RCLCPP_COMPONENTS_REGISTER_NODE(bt_ros_example::BtRosNode)
